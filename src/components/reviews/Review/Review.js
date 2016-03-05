c9716d42f8b7bdf153ec32196a60f8bbce11f68d
Ori Drori <ori.drori@playtech.com>
2016-03-05 18:25:57 +0200

    Review - done

--- src/components/reviews/Review/Review.js
+++ src/components/reviews/Review/Review.js
@@ -1,1 +1,22 @@
 'use strict';
+
+require('./review.less');
+
+import React, { PropTypes } from 'react';
+import ReviewType from './reviewType/ReviewType';
+
+const Review = ({ reviewType, reviewContent }) => (
+	<li className="review">
+		<div className="review__type">
+			<ReviewType type={reviewType} />
+		</div>
+		<q className="review__content">{reviewContent}</q>
+	</li>
+);
+
+Review.propTypes = {
+	reviewType: PropTypes.string,
+	reviewContent: PropTypes.string.isRequired
+};
+
+export default Review;
