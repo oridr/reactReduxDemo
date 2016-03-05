2aa75223ee8fabf3454e6a9a837388da4c3fe02e
Ori Drori <ori.drori@playtech.com>
2016-03-06 00:24:58 +0200

    webpack - added url-loader for images

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,10 @@ module.exports = {
 			{
 				test: /\.less$/,
 				loader: 'style!css!less'
+			},
+			{
+				test: /\.(png|jpg)$/,
+				loader: 'url?limit=20480&name=[name].[ext]'
 			}
 		]
 	},
