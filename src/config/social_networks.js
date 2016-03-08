bb70957f486ce45fae48c276a0af6158038fd99b
Ori Drori <ori.drori@playtech.com>
2016-03-08 21:25:32 +0200

    added config

---
+++ src/config/social_networks.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const socials = {
+	facebook: {
+		logo: require('assets/facebook.svg'),
+		connections: 'friends'
+	},
+	twitter: {
+		logo: require('assets/twitter.svg'),
+		connections: 'followers'
+	},
+	linkedin: {
+		logo: require('assets/linkedin.svg'),
+		connections: 'connections'
+	}
+};
+
+export default socials;
