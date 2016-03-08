e2423f6720e34851ef0e7b57fb370e9380488607
Ori Drori <ori.drori@playtech.com>
2016-03-08 13:36:03 +0200

    TabSet - styling changes

--- src/components/reputation/tabSet/TabSet.js
+++ src/components/reputation/tabSet/TabSet.js
@@ -6,21 +6,17 @@ import React, { PropTypes } from 'react';
 
 import Tab from './tab/Tab';
 
-const TabSet = ({ tabs, activeTab, onTabSelect }) => {
-	console.log(activeTab);
-
-	return (
-		<div className="tabSet">
-			{
-				tabs.map((tab) => (
-					<Tab key={ tab.name }
-						 name={ tab.name }
-						 active={ tab.name === activeTab }
-						 onTabSelect={ onTabSelect }/>
-				))
-			}
-		</div>
-	);
-};
+const TabSet = ({ tabs, activeTab, onTabSelect }) => (
+	<div className="tabSet">
+		{
+			tabs.map((tab) => (
+				<Tab key={ tab.name }
+					 name={ tab.name }
+					 active={ tab.name === activeTab }
+					 onTabSelect={ onTabSelect }/>
+			))
+		}
+	</div>
+);
 
 export default TabSet;
