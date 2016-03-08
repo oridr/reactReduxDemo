158cf42349d2a661d28126373d1b3c5e7618dc30
Ori Drori <ori.drori@playtech.com>
2016-03-08 14:03:57 +0200

    Reputation - done

--- src/components/reputation/Reputation.js
+++ src/components/reputation/Reputation.js
@@ -1,16 +1,38 @@
 'use strict';
 
-import React, { PropTypes } from 'react';
-import RatingMeter from './ratingMeter/ratingMeter';
+require('./reputation.less');
 
-const Reputation = () => (
-	<section>
-		<RatingMeter rating={3.5} maxRating={5} />
-	</section>
-);
+import React, { PropTypes } from 'react';
+import TabSet from './tabSet/TabSet';
+import RatingStars from './ratingStars/RatingStars';
+import Characteristics from './characteristics/Characteristics';
+
+const Reputation = ({ tabs, activeTab, onTabSelect }) => {
+	const currentTab = tabs.find((tab) => tab.name === activeTab);
+
+	return (
+		<section className="reputation">
+			<div className="reputation__tabs">
+				<TabSet tabs={ tabs } activeTab={ activeTab } onTabSelect={onTabSelect} />
+			</div>
+
+			<RatingStars score={ currentTab.score } maxScore={ 100 } />
+
+			<div className="reputation__transactions">
+				Number of Reviews made: { currentTab.total_reviews }
+			</div>
+
+			<div className="reputation__chars">
+				<Characteristics stats={ currentTab.characteristics } />
+			</div>
+		</section>
+	);
+}
 
 Reputation.propTypes = {
-
+	tabs: PropTypes.array.isRequired,
+	activeTab: PropTypes.string.isRequired,
+	onTabSelect: PropTypes.func.isRequired
 };
 
 export default Reputation;
