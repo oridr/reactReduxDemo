dfe1069df6b64759d02a66daa2ace233e7131a7a
Ori Drori <ori.drori@playtech.com>
2016-03-09 20:51:27 +0200

    wrap add in provider

--- src/index.js
+++ src/index.js
@@ -4,9 +4,27 @@ require('./reset.css');
 
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+
+import store from './state/store';
 
 import Widget from './components/widget/Widget';
 
+store.subscribe(() => console.log(store.getState()));
+
+window.fetch('http://api.erated.co/v1/users/4d24120531ac6988984570a018e07a187fdc786d?partner=12341234&mode=marketplaces', {
+		method: 'get'
+	})
+	.then((response) => response.json())
+	.then((result) => store.dispatch({
+		type: 'INIT',
+		payload: result.data
+	}));
+
 const appRoot = document.getElementById('widget');
 
-render(<Widget />, appRoot);
+render(
+	<Provider store={store}>
+		<Widget />
+	</Provider>,
+	appRoot);
