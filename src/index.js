80ae2f5f7e0fb663952c8810d389a6a3deb2d955
Ori Drori <ori.drori@playtech.com>
2016-03-09 23:57:42 +0200

    index - done

--- src/index.js
+++ src/index.js
@@ -6,20 +6,16 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 
-import store from './state/store';
+import store from 'state/store';
+import { initState } from 'state/actionCreators';
 
 import Widget from './components/widget/Widget';
-
-store.subscribe(() => console.log(store.getState()));
 
 window.fetch('http://api.erated.co/v1/users/4d24120531ac6988984570a018e07a187fdc786d?partner=12341234&mode=marketplaces', {
 		method: 'get'
 	})
 	.then((response) => response.json())
-	.then((result) => store.dispatch({
-		type: 'INIT',
-		payload: result.data
-	}));
+	.then((result) => store.dispatch(initState(result.data)));
 
 const appRoot = document.getElementById('widget');
 
@@ -27,4 +23,5 @@ render(
 	<Provider store={store}>
 		<Widget />
 	</Provider>,
-	appRoot);
+	appRoot
+);
