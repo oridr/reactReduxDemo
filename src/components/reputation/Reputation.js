4c8bb0606c23969d5f98f42f0f781033f49cae2b
Ori Drori <ori.drori@playtech.com>
2016-03-09 22:40:27 +0200

    Reputation - wrapped in smart component

--- src/components/reputation/Reputation.js
+++ src/components/reputation/Reputation.js
@@ -3,36 +3,38 @@
 require('./reputation.less');
 
 import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
+
 import TabSet from './tabSet/TabSet';
 import RatingStars from './ratingStars/RatingStars';
 import Characteristics from './characteristics/Characteristics';
 
-const Reputation = ({ tabs, activeTab, onTabSelect }) => {
-	const currentTab = tabs.find((tab) => tab.name === activeTab);
+export const Reputation = ({ tabs, activeTab, onTabSelect }) => (
+	<section className="reputation">
+		<div className="reputation__tabs">
+			<TabSet />
+		</div>
 
-	return (
-		<section className="reputation">
-			<div className="reputation__tabs">
-				<TabSet tabs={ tabs } activeTab={ activeTab } onTabSelect={onTabSelect} />
-			</div>
+		<RatingStars score={ activeTab.score } maxScore={ 100 }/>
 
-			<RatingStars score={ currentTab.score } maxScore={ 100 } />
+		<div className="reputation__transactions">
+			Number of Reviews made: { activeTab.total_reviews }
+		</div>
 
-			<div className="reputation__transactions">
-				Number of Reviews made: { currentTab.total_reviews }
-			</div>
-
-			<div className="reputation__chars">
-				<Characteristics stats={ currentTab.characteristics } />
-			</div>
-		</section>
-	);
-}
+		<div className="reputation__chars">
+			<Characteristics stats={ activeTab.characteristics }/>
+		</div>
+	</section>
+);
 
 Reputation.propTypes = {
 	tabs: PropTypes.array.isRequired,
-	activeTab: PropTypes.string.isRequired,
-	onTabSelect: PropTypes.func.isRequired
+	activeTab: PropTypes.object.isRequired
 };
 
-export default Reputation;
+const mapStateToProps = ({ relevant_reputation, currentReputation}) => ({
+	tabs: relevant_reputation,
+	activeTab: relevant_reputation[currentReputation]
+});
+
+export default connect(mapStateToProps)(Reputation);
