8d04c8e48c681e5b6ee5066190459a0af6dd8e9f
Ori Drori <ori.drori@playtech.com>
2016-03-09 22:30:17 +0200

    mainReducer - added select store

--- src/state/mainReducer.js
+++ src/state/mainReducer.js
@@ -2,16 +2,24 @@
 
 import { INIT, SELECT_STORE, PAGE_REVIEWS } from './actionTypes';
 
-const init = ({ payload }) => ({
+const init = (payload) => ({
 	...payload,
 	reviewPage: 0,
 	currentReputation: 0
 });
 
+const selectStore = (state, payload) => Object.assign({}, state, {
+	reviewPage: 0,
+	currentReputation: payload.currentReputationIndex
+});
+
 const mainReducer = (state = { loading: true }, action) => {
 	switch(action.type) {
 		case INIT:
-			return init(action);
+			return init(action.payload);
+
+		case SELECT_STORE:
+			return selectStore(state, action.payload);
 
 		default:
 			return state;
